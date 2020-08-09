a6b58e36f087c01f368abf6701269a68bb0c5af4
shinkun <shinkunlog@gmail.com>
2020-08-09 18:53:54 +0900

    create dynamic search form

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import client from './client'
 import { ME } from './graphql'
 import { SEARCH_REPOSITORIES } from './graphql'
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -18,14 +18,31 @@ const VARIABLES = {
 class App extends Component {
     constructor(props) {
         super(props)
-        this.state = VARIABLES
+        this.state = DEFAULT_STATE
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            ...DEFAULT_STATE,
+            query: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
     }
 
     render() {
         const { query, first, last, before, after } = this.state
-
+        console.log({query})
         return(
             <ApolloProvider client={client}>
+                <form onSubmit={this.handleSubmit}>
+                    <input value={query} onChange={this.handleChange}/>
+                </form>
                 <Query
                     query={SEARCH_REPOSITORIES}
                     variables={{ query, first, last, before, after }}
