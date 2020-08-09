af097de19dfafebe2643d4ce0c6b7f4bd7975eff
shinkun <shinkunlog@gmail.com>
2020-08-09 17:11:33 +0900

    setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react'
+import { ApolloProvider } from 'react-apollo'
+import { Query } from 'react-apollo'
+// import { gql } from 'graphql-tag'
+import { gql } from 'apollo-boost'
+
+import client from './client'
 
 
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
-console.log({GITHUB_TOKEN})
 
+
+const ME = gql`
+    query me {
+        user(login: "supershinkun") {
+            name
+            avatarUrl
+        }
+    }
+`
 
 class App extends Component {
     render() {
         return(
-            <div>Hello Graphql</div>
+            <ApolloProvider client={client}>
+                <div>Hello Graphql</div>
+
+                <Query query={ME}>
+                    {
+                        ({loading, error, data}) => {
+                            if (loading) return 'Loading...'
+                            if (error) return `Error ${error.message}`
+
+                            return <div>data.user.name</div>
+                        }
+                    }
+                </Query>
+            </ApolloProvider>
+
+
         )
     }
 }
@@ -17,3 +45,6 @@ class App extends Component {
 
 
 export default App
+
+
+
